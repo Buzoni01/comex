7914632db89522fc908971a693cf25623afbae12
Buzoni01 <claudinei.buzoni@brxretail.com>
2022-10-07 23:42:22 -0300

    Exercicios Pedido Mais Barato e Mais Caro - Semana 8

--- src/br/com/comex/csv/PedidoMaisBaratoEMaisCaro.java
+++ src/br/com/comex/csv/PedidoMaisBaratoEMaisCaro.java
@@ -1,10 +1,38 @@
 package br.com.comex.csv;
 
+import java.io.FileNotFoundException;
+import java.util.Comparator;
+import java.util.List;
+
 public class PedidoMaisBaratoEMaisCaro {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		LeitorPedidosCsv leitorPedidosCsv = new LeitorPedidosCsv();
+		List<PedidoCsv> leArquivoCsv = leitorPedidosCsv.leArquivoCsv();
+		
+		ComparadorDePedidos porValor = new ComparadorDePedidos();
+		leArquivoCsv.sort(porValor);
+		System.out.println("Pedido Mais Barato: "+leArquivoCsv.get(0));
+		System.out.println("Pedido Mais Caro  : "+leArquivoCsv.get(leArquivoCsv.size()-1));
 	}
 
 }
+
+class ComparadorDePedidos implements Comparator<PedidoCsv> {
+
+	@Override
+	public int compare(PedidoCsv p1, PedidoCsv p2) {
+		double ValorP1 = Double.parseDouble(p1.getPreco()) * Double.parseDouble(p1.getQuantidade());
+		double ValorP2 = Double.parseDouble(p2.getPreco()) * Double.parseDouble(p2.getQuantidade());
+		if(ValorP1 == ValorP2 ){
+			return 0;
+		} else if(ValorP1 > ValorP2 ){
+			return 1;
+		} else {
+		    return -1;
+		}
+	}
+}	
+
+
+	
