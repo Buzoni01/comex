4eb9ba2c977025874e9bef2a191c9f7aa9d7d33c
Buzoni01 <claudinei.buzoni@brxretail.com>
2022-10-26 17:36:25 -0300

    Volumes no container do Oracle - Semana11

--- oracle-volume/oracle.sh
+++ oracle-volume/oracle.sh
@@ -1,3 +1,5 @@
 clear
-docker run -d --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
-docker container logs oracle
+# docker run -d --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+# docker container logs oracle
+docker volume create oracle-volume
+docker run -d -v oracle-volume:/opt/oracle/oradata --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
