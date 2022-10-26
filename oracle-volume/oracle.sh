bb60335b9645b0773fd0ce9ed91df1fa6a798dec
Buzoni01 <claudinei.buzoni@brxretail.com>
2022-10-26 14:31:19 -0300

    Gerando um JAR executável para o Comex - Semana11

--- oracle-volume/oracle.sh
+++ oracle-volume/oracle.sh
@@ -1,3 +1,3 @@
 clear
-docker run -d --name oracle -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker run -d --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
 docker container logs oracle
--- src/br/com/comex/modelo/ConnectionFactory.java
+++ src/br/com/comex/modelo/ConnectionFactory.java
@@ -1,13 +1,33 @@
 package br.com.comex.modelo;
 
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class ConnectionFactory {
+////	public Connection Conect(){ //throws SQLException {
+//	public Connection recuperarConexao(){ //throws SQLException {
+//		try {return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "brx");} 
+//		catch (SQLException e) {throw new RuntimeException(e);}	
+//	}
+//}
+
+
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-//	public Connection Conect(){ //throws SQLException {
+
+
 	public Connection recuperarConexao(){ //throws SQLException {
-		try {return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "brx");} 
-		catch (SQLException e) {throw new RuntimeException(e);}	
-	}
-}
+
+		try {
+			return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "brx");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	
+}}
